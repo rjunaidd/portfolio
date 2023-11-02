8f6090c6327ba3d850334c3a5eef60b79eceafc3
rjunaidd <roafjunaid09@gmail.com>
2023-11-02 10:12:01 -0700

    skill

--- lib/Screens/skill_screen.dart
+++ lib/Screens/skill_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/Data/app_data.dart';
 import 'package:portfolio/Utils/app_strings.dart';
+import 'package:portfolio/Widgets/progress_bar.dart';
 
 class SkillScreen extends StatelessWidget {
   const SkillScreen({super.key});
@@ -11,10 +13,28 @@ class SkillScreen extends StatelessWidget {
         title:const Text("My Skills"),
         backgroundColor: Colors.grey.shade700,
       ),
-      body:const Center(
+      body: Center(
         child: Column(
           children: [
-            Image(image: AssetImage(AppString.SkillPic))
+            const Image(image: AssetImage(AppString.SkillPic)),
+            Card(
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)
+              ),
+              color: Colors.grey.shade300,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: List.generate(AppData.modelData.skillAndProgress.length,
+                        (index) => ProgressBar(
+                        progresstitle: AppData.modelData.skillAndProgress[index].name,
+                        progresss: AppData.modelData.skillAndProgress[index].progress
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
